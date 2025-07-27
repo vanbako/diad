ac5e23be5f6e8a09dfc5f0a22dac1838cb38e88a
vanbako <github@datix.be>
2025-07-27 23:45:21 +0200

    improved tests.bash

--- tst/bash/tests.bash
+++ tst/bash/tests.bash
@@ -1,11 +1,11 @@
-OPCO="MOVu"
-TICKS=15
-iverilog -DDEBUGOPC -DDEBUGGP -DDEBUGFLAGS -DTICKS=${TICKS} -DMEM_HEX_FILE="\"tst/hex/${OPCO}.hex\"" -g2012 -o test.vvp src/*.v
-vvp test.vvp | grep -v -e "WARNING" -e '\$finish' | tail -n +13 >tst/out/${OPCO}.out
-sdiff tst/out/${OPCO}.out tst/dif/${OPCO}.out >/dev/null && echo "${OPCO} Ok" || echo "${OPCO} Bad"
-
-OPCO="ADDu"
-TICKS=15
-iverilog -DDEBUGOPC -DDEBUGGP -DDEBUGFLAGS -DTICKS=${TICKS} -DMEM_HEX_FILE="\"tst/hex/${OPCO}.hex\"" -g2012 -o test.vvp src/*.v
-vvp test.vvp | grep -v -e "WARNING" -e '\$finish' | tail -n +13 >tst/out/${OPCO}.out
-sdiff tst/out/${OPCO}.out tst/dif/${OPCO}.out >/dev/null && echo "${OPCO} Ok" || echo "${OPCO} Bad"
+#!/bin/bash
+tests=(
+  "MOVu 15"
+  "ADDu 15"
+)
+for test in "${tests[@]}"; do
+  read OPCO TICKS <<< "$test"
+  iverilog -DDEBUGOPC -DDEBUGGP -DDEBUGFLAGS -DTICKS=${TICKS} -DMEM_HEX_FILE="\"tst/hex/${OPCO}.hex\"" -g2012 -o test.vvp src/*.v
+  vvp test.vvp | grep -v -e "WARNING" -e '\$finish' | tail -n +13 >tst/out/${OPCO}.out
+  sdiff tst/out/${OPCO}.out tst/dif/${OPCO}.out >/dev/null && echo "${OPCO} Ok" || echo "${OPCO} Bad"
+done
