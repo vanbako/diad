f0828c9289e665208a6cece7b848fb611eeb8f9a
vanbako <github@datix.be>
2025-07-27 23:30:45 +0200

    Testset for ADDu

--- tst/bash/tests.bash
+++ tst/bash/tests.bash
@@ -3,3 +3,9 @@ TICKS=15
 iverilog -DDEBUGOPC -DDEBUGGP -DDEBUGFLAGS -DTICKS=${TICKS} -DMEM_HEX_FILE="\"tst/hex/${OPCO}.hex\"" -g2012 -o test.vvp src/*.v
 vvp test.vvp | grep -v -e "WARNING" -e '\$finish' | tail -n +13 >tst/out/${OPCO}.out
 sdiff tst/out/${OPCO}.out tst/dif/${OPCO}.out >/dev/null && echo "${OPCO} Ok" || echo "${OPCO} Bad"
+
+OPCO="ADDu"
+TICKS=15
+iverilog -DDEBUGOPC -DDEBUGGP -DDEBUGFLAGS -DTICKS=${TICKS} -DMEM_HEX_FILE="\"tst/hex/${OPCO}.hex\"" -g2012 -o test.vvp src/*.v
+vvp test.vvp | grep -v -e "WARNING" -e '\$finish' | tail -n +13 >tst/out/${OPCO}.out
+sdiff tst/out/${OPCO}.out tst/dif/${OPCO}.out >/dev/null && echo "${OPCO} Ok" || echo "${OPCO} Bad"
